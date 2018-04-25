446d5e43487b472c3d516fcfa0b6b171d6bd466a
xumiao <331450663@qq.com>
2018-04-25 11:03:06 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -3,18 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-//import  VueResource  from 'vue-resource'
-//Vue.use(VueResource) 
-//Vue.http.options.emulateJSON = true   //Web服务器无法处理编码为application/json的请求，可以启用emulateJSON选项。请求会以application/x-www-form-urlencoded作为MIME type，就像普通的HTML表单一样。
 
 import axios from 'axios'
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
-import '!style-loader!css-loader!less-loader!./style/global.css' //es6引入全局css
-//require('!style-loader!css-loader!less-loader!./style/global.css') //es5引入全局css
-
+import '!style-loader!css-loader!less-loader!./style/global.css'
 
 /* eslint-disable no-new */
 new Vue({
